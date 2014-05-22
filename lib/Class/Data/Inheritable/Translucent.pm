ac5b5962d7650f9083ddd0563727bfd6932860c0
Steve Hay <steve.m.hay@googlemail.com>
2014-05-22 08:53:48 +0100

    Bump version for next release

--- lib/Class/Data/Inheritable/Translucent.pm
+++ lib/Class/Data/Inheritable/Translucent.pm
@@ -11,7 +11,7 @@ Class::Data::Inheritable::Translucent - Inheritable, overridable, translucent cl
 
 =cut
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 if (eval { require Sub::Name }) {
     Sub::Name->import;
